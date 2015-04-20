c25994db5e7da04c0dc4d37e5c7f0c5cb8224b5b
indasali <indasali@sheridanc.on.ca>
2015-04-20 10:32:53 -0400

    Added a comment about the file

--- index.js
+++ index.js
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+ /* This is a comment for an example for Hybrid Mobile*/
 var app = {
     // Application Constructor
     initialize: function() {
